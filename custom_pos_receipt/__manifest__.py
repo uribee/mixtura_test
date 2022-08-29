8e80d7bb3e61afeeab69d364673eee5f7932d4b2
Expertha <100881758+Expertha@users.noreply.github.com>
2022-08-29 10:53:25 -0600

    agregando ticket

---
+++ custom_pos_receipt/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Custom pos receipt',
+    'category': 'Sales/Point of Sale',
+    'summary': 'This module is used to customized receipt of point of sale when a user adds a product in the cart and validates payment and print receipt, then the user can see the client name on POS Receipt. | Custom Receipt | POS Reciept | Payment | POS Custom Receipt',
+    'description': "Customized our point of sale receipt",
+    'version': '15.0.1.0',
+    'depends': ['base', 'point_of_sale'],
+    'assets': {
+        'web.assets_qweb': [
+            "custom_pos_receipt/static/src/xml/cliente.xml",
+            "custom_pos_receipt/static/src/xml/pos.xml",
+        ]
+    },
+    'installable': True,
+}
